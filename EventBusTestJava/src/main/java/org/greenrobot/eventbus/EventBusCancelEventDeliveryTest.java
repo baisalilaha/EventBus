36918e5e1b5062f936ac1fd0c0dadee0e528e587
Baisali Laha <baisali@MacBook-Air.cogeco.local>
2024-04-23 16:16:57 -0400

    this is a test

--- EventBusTestJava/src/main/java/org/greenrobot/eventbus/EventBusCancelEventDeliveryTest.java
+++ EventBusTestJava/src/main/java/org/greenrobot/eventbus/EventBusCancelEventDeliveryTest.java
@@ -23,6 +23,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
+/**
+ * Some test file for git hub
+  **/
 public class EventBusCancelEventDeliveryTest extends AbstractEventBusTest {
 
     Throwable failed;
